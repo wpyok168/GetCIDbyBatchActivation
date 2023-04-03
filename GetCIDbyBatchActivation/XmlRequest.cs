887f0c7ae8fc8517987c71e91ce88f22fd8f70cf
wpyok500 <wpyok500@gmail.com>
2023-04-03 14:57:30 +0800

    up

--- GetCIDbyBatchActivation/XmlRequest.cs
+++ GetCIDbyBatchActivation/XmlRequest.cs
@@ -27,12 +27,15 @@ namespace GetCIDbyBatchActivation
 
             XNamespace xn = "http://www.microsoft.com/DRM/SL/BatchActivationResponse/1.0";
             XElement xe1 = xd1.Descendants(xn+ "CID").FirstOrDefault();
+            XElement errr = xd1.Descendants(xn + "ErrorCode").FirstOrDefault();
 
             XElement xe2 = xd1.Root.Element(xn+ "Responses").Element(xn + "Response").Element(xn + "CID");
 
             XmlNamespaceManager xnm = new XmlNamespaceManager(new NameTable());
-            xnm.AddNamespace("ActivationResponse", "http://www.microsoft.com/DRM/SL/BatchActivationResponse/1.0");
-            XElement xe3 = xd1.XPathSelectElement("//ActivationResponse:CID", xnm);
+            xnm.AddNamespace("ar", "http://www.microsoft.com/DRM/SL/BatchActivationResponse/1.0");
+            XElement xe3 = xd1.XPathSelectElement("//ar:CID", xnm);
+            XElement err = xd1.XPathSelectElement("//ar:ErrorCode", xnm);
+            string errstr = err.Value;
 
         }
         private static XDocument CreateXml(int typeid, string iid, string pid)
